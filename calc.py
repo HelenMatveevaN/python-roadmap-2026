927c459351dace6b025bac97754763ffa450baf9
Elena Matveeva <helen.matveeva.n@gmail.com>
2026-09-24 12:05:45 +0300

    feat: добавлена защита JSON и исправлен порядок вывода истории

--- calc.py
+++ calc.py
@@ -34,8 +34,8 @@ OPS = {
     "%": operator.mod,
 }
 
-prior_ops = ["*","/","//","/"]
-other_ops = ["+","-"]
+prior_ops = ["*", "/", "//", "%"]
+other_ops = ["+", "-"]
 
 argument = sys.argv[1:]
 
@@ -53,12 +53,16 @@ match argument:
 
         if os.path.exists(file_name):
             with open(file_name, "r", encoding="utf-8") as f:
-                history = json.load(f)
+                try:
+                    history = json.load(f)
+                except json.JSONDecodeError:
+                    # Если файл поврежден, не падаем, а просто начинаем историю с чистого листа
+                    history = []
 
         if history: # Если список НЕ пустой
             print(f"Ваша история вычислений:")
             for item in history[-10:]: # последние 10
-                print(f" -> {item}")
+                print(f" • {item}")
         else:
             print("История вычислений пуста.")
 
@@ -66,41 +70,75 @@ match argument:
     case [expression]:
         print(f"Считаем выражение: {expression}")
 
+        if os.path.exists(file_name):
+            with open(file_name, "r", encoding="utf-8") as f:
+                try:
+                    history = json.load(f)
+                except json.JSONDecodeError:
+                    # Если файл поврежден, не падаем, а просто начинаем историю с чистого листа
+                    history = []
+
         explist = expression.split() #get list
+
+        if len(explist) < 3:
+            print("Ошибка ввода выражения. \nПример ввода: python calc.py \"выражение\"")
+            sys.exit()
+
         result = 0
 
-        while any(op in explist for op in prior_ops):
-            indices = {op: explist.index(op) for op in prior_ops if op in explist}
+        try: 
+            while any(op in explist for op in prior_ops):
+                indices = {op: explist.index(op) for op in prior_ops if op in explist}
 
-            sign = min(indices, key=indices.get) #мин.оператор слева
-            index = indices[sign]
+                sign = min(indices, key=indices.get) #мин.оператор слева
+                index = indices[sign]
 
-            leftnum = Decimal(str(explist[index-1]))
-            rightnum = Decimal(str(explist[index+1]))
+                leftnum = Decimal(str(explist[index-1]))
+                rightnum = Decimal(str(explist[index+1]))
 
-            prior_func = OPS[sign]
-            prior_result = prior_func(leftnum, rightnum)
+                prior_func = OPS[sign]
+                prior_result = prior_func(leftnum, rightnum)
 
-            explist[index-1:index+2] = [prior_result]
-            print(f"explist = {explist}")
+                explist[index-1:index+2] = [prior_result]
+                #print(f"explist = {explist}")
 
-        while any(op in explist for op in other_ops):
-            indices = {op: explist.index(op) for op in other_ops if op in explist}
-            
-            sign = min(indices, key=indices.get)
-            index = indices[sign]
+            while any(op in explist for op in other_ops):
+                indices = {op: explist.index(op) for op in other_ops if op in explist}
+                
+                sign = min(indices, key=indices.get)
+                index = indices[sign]
 
-            leftnum = Decimal(str(explist[index-1]))
-            rightnum = Decimal(str(explist[index+1]))
+                leftnum = Decimal(str(explist[index-1]))
+                rightnum = Decimal(str(explist[index+1]))
 
-            other_func = OPS[sign]
-            result = other_func(leftnum, rightnum)
+                other_func = OPS[sign]
+                result = other_func(leftnum, rightnum)
 
-            explist[index-1:index+2] = [result]
-            print(f"explist = {explist}")
+                explist[index-1:index+2] = [result]
+                #print(f"explist = {explist}")
 
-        result = explist[0]
-        print(f"Результат: {result}")
+            result = explist[0]
+            print(f"Результат: {result}")
+
+            history.append(f"{expression} = {result}")
+
+            if len(history) > 50:
+                del history[0]
+            with open(file_name, "w", encoding="utf-8") as f:
+                json.dump(history, f)
+        
+        except ZeroDivisionError:
+            print("Ошибка: деление на ноль невозможно!")
+            sys.exit()
+
+        except ValueError:
+            print("Ошибка: неверный формат чисел (например, ввели буквы вместо цифр)!")
+            sys.exit()
+
+        except Exception as e:
+            # Этот блок поймает любую другую неизвестную ошибку
+            print(f"Произошла непредвиденная ошибка: {e}")
+            sys.exit()
 
     case _:
         print("Как пользоваться: python calc.py \"выражение\" или --history / --clear")
